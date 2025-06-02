273dd7af968e0afcd73e3cd26596756faf8a3df2
Hevitriniavo <hei.tantely@gmail.com>
2025-06-02 20:51:20 +0300

    refactor: improve header layout and button styling

--- src/components/layouts/AdminLayout.vue
+++ src/components/layouts/AdminLayout.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
     <nav class="fixed bg-white top-0 left-0 right-0  shadow z-50 flex justify-between items-center px-10 py-6">
-         <img
-           :src="logo"
-           alt="Logo"
-           class="h-8 w-auto"
-         />
+      <div class="flex items-center space-x-2">
+        <img
+          :src="logo"
+          alt="Synergy Group Logo"
+          class="h-6 w-auto"
+        />
+        <div class="flex flex-row uppercase  text-lg font-semibold">
+          <span class="text-dark-blue-text ">synergy</span>
+          <span class="text-[#f6a11b]">group</span>
+        </div>
+      </div>
+
+
 
       <div class="flex justify-center space-x-4">
         <the-button btn-class="bg-header-menu-bg text-dark-blue-text shadow-custom-card rounded-lg">
@@ -32,5 +40,5 @@
 <script setup>
 import TheButton from '@/components/ui/TheButton.vue'
 import { LogOut } from 'lucide-vue-next'
-import logo from  '@/assets/images/logo/Logo Synergy.png'
+import logo from  '@/assets/images/icones/logo_seul_synergygroup_color.png'
 </script>
